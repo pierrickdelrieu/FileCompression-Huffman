15b01cb366f88bc90388c939502f33a1cbea1644
pierrickdelrieu <pierrick.delrieu@efrei.net>
2020-11-08 09:29:25 +0100

    ajout des fonctions empiler et depiler

--- sources/DataStructures/File.c
+++ sources/DataStructures/File.c
@@ -12,53 +12,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../include/DataStructures/File.h"
-#include "../../include/DataStructures/LinkedList.h"
 
 
-
-void init_file(File** f) {
+/**
+ * @brief Initialization of the start and end fields of the fiel (queue) and memory allocation
+ * 
+ * @param f File (queue) address initialized file initialized (modified data)
+ */
+void initFile(File** f) {
     *f = (File*) malloc(sizeof(File));
     (*f)->start = NULL;
     (*f)->end = NULL;
 }
 
 
-void enfiler(File** f, Node* nx_elem) {
+/**
+ * @brief Adding an node to the top of the list
+ * 
+ * @param f File (queue) adress (modified data) where the node must be added
+ * @param nx_elem Node to add at the top of the list
+ * @return int Return 1 if node has been added and 0 otherwise (if the file (queue) has not been initialized)
+ */
+int enfiler(File** f, Node* nx_elem) {
+    // If the file (queue) has been initialized
     if (f != NULL) {
 
+        // If the file (queue) is not empty
         if (((*f)->start == NULL) && ((*f)->end == NULL)) {
             (*f)->start = nx_elem;
             (*f)->end = nx_elem;
+            return 1;
         }
+
+        // If the file (queue) already contains nodes
         else {
             Node *temp = (*f)->start;
             (*f)->start = nx_elem;
             nx_elem->right = temp;
+            return 1;
         }
     }
+    
+    // If the file (queue) has not been initialized
+    return 0;
 }
 
 
+/**
+ * @brief Remove and return the last element in the file (queue)
+ * 
+ * @param f File (queue) adress (modified data) where the node must be removed
+ * @return Node* Removed node
+ */
 Node* defiler(File** f) {
+    // If the file (queue) has been initialized
     if(f != NULL) {
-        Element* temp = (*f)->start;
+        Node* temp = (*f)->start;
 
+        // If the file (queue) contains one or zero node
         if((*f)->start == (*f)->end){
             (*f)->start = NULL;
             (*f)->end = NULL;
-            return temp->data;
+            return temp->letter;
         }
+
+        // If the file (queue) contains several nodes
         else {
-            Element* ptrprec = NULL;
-            while (temp->next != NULL) {
+            Node* ptrprec = NULL;
+
+            // Placement at the last and penultimate node of the list
+            while (temp->right != NULL) {
                 ptrprec = temp;
-                temp = temp->next;
+                temp = temp->right;
             }
 
             (*f)->end = ptrprec;
-            (*f)->end->next = NULL;
-            return temp->data;
+            (*f)->end->right = NULL;
+            return temp->letter;
         }
     }
+
+    // If the file (queue) has not been initialized
     return NULL;
 }
