3c2e09fad4c7bfdb4b9a8c94e6bee00821277f75
Benjamin Lesieux <benjamin.lesieux@efrei.net>
2020-11-29 12:50:30 +0100

    Added the skeleton for Decoding.c and Decoding.h

--- include/Huffman/Decoding.h
+++ include/Huffman/Decoding.h
@@ -1,5 +1,14 @@
 #ifndef DECODING
 #define DECODING
 
+#include "../../include/Huffman/HuffmanTree.h"
+
+void            decodeFile              (void);
+
+HuffmanTree     buildFromDictionary     (FILE* file);
+
+unsigned int    getCodeSizeMax          (FILE* file);
+
+void            buildFromCode           (HuffmanTree tree, char letter, char* code, int index, int sizeCode);
 
 #endif // !DECODING
